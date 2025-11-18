70cd906ec3ac3ba5495596433d1e889dddc11dea
Pandi <pandiviris5@gmail.com>
2025-11-17 23:43:00 -0500

    Funcionabilidad de los tres botones para interactuar con la cinta

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 let form = document.getElementById("formCinta");        // Formulario para obtener el array
 let cajaCinta = document.getElementById("tapeBox");     // Div que representa la cinta en el index.html
-let oneStep = document.getElementById("stepBtn");       // Boton para avanazar en la cinta una casilla
+
+let oneStep = document.getElementById("stepBtn");       // Boton para avanazar en la cinta una vez
+let autoStep = document.getElementById("autoBtn");     // Boton para avance automatico de la cinta
+let reset = document.getElementById("resetBtn");        // Boton para reiniciar la cinta
+
 let regex = /^(\d{4}|\d{6})$/;                          // Expresion regex a validar
+
 cinta = [];                                             // Arreglo global que representa la cinta
 let head = 0;                                           // Cabecera de la cinta
 
@@ -35,9 +40,44 @@ form.addEventListener("submit", (e) =>{
     const valor = inputCinta.value.trim();
 
     if(regex.test(valor)){
-        let cinta = valor.split("");
+        head = 0;
+        cinta = valor.split("");
         renderCinta(cinta, head);
     }else {
+        cinta = [];
         cajaCinta.innerHTML = "<span class=text-muted>Cadena no válida</span>";
     }
+})
+
+oneStep.addEventListener("click", ()=>{
+    let tamano = cinta.length;
+    if ((tamano > 0) && (head < tamano-1)) {
+        head++;
+        renderCinta(cinta, head);
+    }
+})
+
+reset.addEventListener("click", ()=>{
+    let tamano = cinta.length;
+    if (tamano > 0) {
+        head = 0;
+        renderCinta(cinta, head);
+    }
+})
+
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function autoRun() {
+    let tamano = cinta.length;
+    while ((tamano > 0) && (head < tamano - 1)) {
+        await delay(3000); // 3 segundo entre pasos
+        head++;
+        renderCinta(cinta, head);
+    }
+}
+
+autoStep.addEventListener("click", ()=>{
+    autoRun();
 })
