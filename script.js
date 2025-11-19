1385a01740f11e1b469024212aff1c3720b2e63e
Pandi <pandiviris5@gmail.com>
2025-11-18 22:15:53 -0500

    Correccion de logica funcional de la MT y agregacion de indicador visual (color verde o rojo) para saber si una cadena es valida o no

--- script.js
+++ script.js
@@ -2,15 +2,84 @@ let form = document.getElementById("formCinta");        // Formulario para obten
 let cajaCinta = document.getElementById("tapeBox");     // Div que representa la cinta en el index.html
 
 let oneStep = document.getElementById("stepBtn");       // Boton para avanazar en la cinta una vez
-let autoStep = document.getElementById("autoBtn");     // Boton para avance automatico de la cinta
+let autoStep = document.getElementById("autoBtn");      // Boton para avance automatico de la cinta
 let reset = document.getElementById("resetBtn");        // Boton para reiniciar la cinta
 
-let regex = /^(\d{4}|\d{6})$/;                          // Expresion regex a validar
+let regex = /^[\d_]*$/;                                    // Expresion regex a validar
+let posDecision = null;
 
 cinta = [];                                             // Arreglo global que representa la cinta
 let head = 0;                                           // Cabecera de la cinta
+let estado = "q0";                                      // Estado inicial de la MT
+let alfabeto = ["1","2","3","4","5","6","7","8","9","_"];
 
-function renderCinta(cinta, head) {
+const transiciones = {
+    "q0"        : {},
+    "q1"        : {},
+    "q2"        : {},
+    "q3"        : {},
+    "q4"        : {},
+    "q5"        : {},
+    "q6"        : {},
+    "qaccept"   : {},
+    "qreject"   : {},
+};
+
+alfabeto.forEach(simbolo => {
+    if (simbolo != "_") 
+        transiciones["q0"][simbolo] = {escribir : simbolo, mover : "R", sigEstado : "q1"};
+    else
+        transiciones["q0"][simbolo] = {escribir : simbolo, mover : "S", sigEstado : "qreject"};
+});
+
+alfabeto.forEach(simbolo => {
+    if (simbolo != "_") 
+        transiciones["q1"][simbolo] = {escribir : simbolo, mover : "R", sigEstado : "q2"};
+    else
+        transiciones["q1"][simbolo] = {escribir : simbolo, mover : "S", sigEstado : "qreject"};
+});
+
+alfabeto.forEach(simbolo => {
+    if (simbolo != "_") 
+        transiciones["q2"][simbolo] = {escribir : simbolo, mover : "R", sigEstado : "q3"};
+    else
+        transiciones["q2"][simbolo] = {escribir : simbolo, mover : "S", sigEstado : "qreject"};
+});
+
+alfabeto.forEach(simbolo => {
+    if (simbolo != "_") 
+        transiciones["q3"][simbolo] = {escribir : simbolo, mover : "R", sigEstado : "q4"};
+    else
+        transiciones["q3"][simbolo] = {escribir : simbolo, mover : "S", sigEstado : "qreject"};
+});
+
+alfabeto.forEach(simbolo => {
+    if (simbolo != "_") 
+        transiciones["q4"][simbolo] = {escribir : simbolo, mover : "R", sigEstado : "q5"};
+    else
+        transiciones["q4"][simbolo] = {escribir : simbolo, mover : "S", sigEstado : "qaccept"};
+});
+
+alfabeto.forEach(simbolo => {
+    if (simbolo != "_") 
+        transiciones["q5"][simbolo] = {escribir : simbolo, mover : "R", sigEstado : "q6"};
+    else
+        transiciones["q5"][simbolo] = {escribir : simbolo, mover : "S", sigEstado : "qreject"};
+});
+
+alfabeto.forEach(simbolo => {
+    if (simbolo != "_") 
+        transiciones["q6"][simbolo] = {escribir : simbolo, mover : "R", sigEstado : "qreject"};
+    else
+        transiciones["q6"][simbolo] = {escribir : simbolo, mover : "S", sigEstado : "qaccept"};
+});
+
+alfabeto.forEach(simbolo => {
+    transiciones["qreject"][simbolo] = {escribir : simbolo, mover : "S", sigEstado : "qreject"};
+    transiciones["qaccept"][simbolo] = {escribir : simbolo, mover : "S", sigEstado : "qaccept"};
+});
+
+function renderCinta() { 
     cajaCinta.innerHTML = "";
 
     cinta.forEach((simbolo, index) => {
@@ -24,9 +93,17 @@ function renderCinta(cinta, head) {
 
         // Validar si es la cabecera de la cinta
         if (index === head) {
-            celda.style.background = "#cce5ff";
+            celda.style.background = "#cce5ff";    
             celda.style.border = "2px solid #007bff";
             celda.style.fontWeight = "bold";
+        }
+
+        // Posicion de decision si es cadena valida o no
+        if(index == posDecision){
+            if(estado == "qaccept")
+                celda.style.background = "#7bfc76ff";
+            else
+                celda.style.background = "#f58888ff";
         }
 
         cajaCinta.appendChild(celda);
@@ -41,28 +118,60 @@ form.addEventListener("submit", (e) =>{
 
     if(regex.test(valor)){
         head = 0;
+        estado = "q0";
+        posDecision = null;
         cinta = valor.split("");
-        renderCinta(cinta, head);
+        renderCinta();
     }else {
         cinta = [];
         cajaCinta.innerHTML = "<span class=text-muted>Cadena no válida</span>";
     }
 })
 
+function step(){
+
+    if (estado === "qaccept" || estado === "qreject") {
+        renderCinta(); 
+        return;
+    }
+
+    console.log(estado);
+    let simboloActual = cinta[head];
+    let regla = transiciones[estado][simboloActual];
+
+    if (!regla) {
+        estado = "qreject"
+        return;
+    }
+
+    if ((regla.sigEstado == "qaccept") ||(regla.sigEstado == "qreject"))
+        posDecision = head;
+
+    // Escribir en cinta
+    cinta[head] = regla.escribir;
+
+    // Mover cinta
+    if (regla.mover == "R")
+        head++;
+
+    // Nuevo estado
+    estado = regla.sigEstado;
+
+    renderCinta();
+}
+
 oneStep.addEventListener("click", ()=>{
     let tamano = cinta.length;
-    if ((tamano > 0) && (head < tamano-1)) {
-        head++;
-        renderCinta(cinta, head);
+    if ((tamano > 0) && (head < tamano)) {
+        step();    
     }
 })
 
 reset.addEventListener("click", ()=>{
-    let tamano = cinta.length;
-    if (tamano > 0) {
-        head = 0;
-        renderCinta(cinta, head);
-    }
+    head = 0;
+    estado = "q0";
+    posDecision = null;
+    renderCinta();
 })
 
 function delay(ms) {
@@ -70,11 +179,9 @@ function delay(ms) {
 }
 
 async function autoRun() {
-    let tamano = cinta.length;
-    while ((tamano > 0) && (head < tamano - 1)) {
-        await delay(3000); // 3 segundo entre pasos
-        head++;
-        renderCinta(cinta, head);
+    while (estado !== "qaccept" && estado !== "qreject") {
+        await delay(3000);
+        step();
     }
 }
 
